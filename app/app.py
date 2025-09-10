3d0bf888b0be20a11fb5cd46dcadf3f995068a7c
Muhammad A Fattah <fattahmuhammad17@gmail.com>
2025-09-10 21:09:38 +0700

    Fix Flask app: define Flask app and route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker (Actions deploy) (Actions deploy) on EC2 via Terraform + GitHub Actions - 2025-09-10 17:30:14!
+    return "Hello from Docker on EC2 via Terraform + GitHub Actions!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
