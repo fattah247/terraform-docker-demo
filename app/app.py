b8dcf0db974319b932810908a06d5f68580667a8
Muhammad A Fattah <fattahmuhammad17@gmail.com>
2025-09-10 17:30:14 +0700

    Trigger deploy

--- app/app.py
+++ app/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker on EC2 via Terraform + GitHub Actions - 2025-09-10 17:27:08!
+    return "Hello from Docker on EC2 via Terraform + GitHub Actions - 2025-09-10 17:30:14!
