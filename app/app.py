095de542f95c223a79d851a53dc845492558b836
Muhammad A Fattah <fattahmuhammad17@gmail.com>
2025-09-10 20:38:05 +0700

    Trigger deploy: change page text

--- app/app.py
+++ app/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker on EC2 via Terraform + GitHub Actions - 2025-09-10 17:30:14!
+    return "Hello from Docker (Actions deploy) on EC2 via Terraform + GitHub Actions - 2025-09-10 17:30:14!
